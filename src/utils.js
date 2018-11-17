e40fa9971a6f4337df3e40cfd69e946ddfa6ad2c
Robert Painsi <robertpainsi@gmail.com>
2018-11-17 13:21:07 +0100

    Improve cube output
    * Colored letters
    * More cube-like sides

--- src/utils.js
+++ src/utils.js
@@ -13,14 +13,27 @@ export const logCube = ($) => {
         result[2][2] = side[9];
         return result;
     };
-    const cube = {
-        F: sideToArray($.F),
-        B: sideToArray($.B),
-        L: sideToArray($.L),
-        R: sideToArray($.R),
-        U: sideToArray($.U),
-        D: sideToArray($.D),
-    };
-    console.log(cube);
+    // const cube = {
+    //     F: sideToArray($.F),
+    //     B: sideToArray($.B),
+    //     L: sideToArray($.L),
+    //     R: sideToArray($.R),
+    //     U: sideToArray($.U),
+    //     D: sideToArray($.D),
+    // };
+    // console.log(cube);
+
+    for (let i = 0; i < 3; i++) {
+        let line = ``;
+        [`F`, `B`, `L`, `R`, `U`, `D`].forEach((sideName) => {
+            line += `   ${(i === 0) ? `${sideName}` : ` `}|${sideToArray($[sideName])[i].join(' ')}|`
+                .replace(/r/g, `\x1b[31mr\x1b[0m`)
+                .replace(/g/g, `\x1b[32mg\x1b[0m`)
+                .replace(/b/g, `\x1b[34mb\x1b[0m`)
+                .replace(/y/g, `\x1b[33my\x1b[0m`)
+                .replace(/o/g, `\x1b[36mo\x1b[0m`);
+        });
+        console.log(line);
+    }
     console.log();
 };
